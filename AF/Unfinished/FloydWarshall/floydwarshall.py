4aaa6acbba4693de8a335cd4b105f402f4bd0e38
Razvan <razvanilie@protonmail.com>
2021-07-11 20:44:44 +0300

    Modified entirely the algorithm so it can be paired with the format used in the other algorithms

--- AF/Unfinished/FloydWarshall/floydwarshall.py
+++ AF/Unfinished/FloydWarshall/floydwarshall.py
@@ -1,94 +1,62 @@
-# Python Program for Floyd Warshall Algorithm
+import sys
+INF = sys.maxsize
 
-# Number of vertices in the graph
-V = 4
+class Graph:
+    def __init__(self, n):
+        self.vertixes = n
+        self.matrix = [[INF for x in range(n)] for y in range(n)] # initialize with infinite first
+        for x in range(n):
+            self.matrix[x][x] = 0 # the distances on diagonal will be 0
+        # now it looks something like this
+        '''
+        0 INF INF INF 
+        INF 0 INF INF 
+        INF INF 0 INF 
+        INF INF INF 0 
+        '''
 
-# Define infinity as the large
-# enough value. This value will be
-# used for vertices not connected to each other
-INF = 999999
-INF2 = 10000
+    def addEdge(self, u, v, w):
+        self.matrix[u][v] = w
+        '''
+        g5.addEdge(0, 1, 5)
+        g5.addEdge(0, 3, 10)
+        g5.addEdge(1, 2, 3)
+        g5.addEdge(2, 3, 1)
+        '''
+        # after the last addition the matrix will look something like this
+        '''
+        0 5 INF 10
+        INF 0 3 INF
+        INF INF 0 1
+        INF INF INF 0
+        '''
+    def printGraph(self):
+        print('\n')
+        for i in range(len(self.matrix)):
+            for j in range(len(self.matrix[0])):
+                if self.matrix[i][j] == sys.maxsize:
+                    print("INF", end=" | ") # it s ugly to show 9223372036854775807, better INF
+                else:
+                    print(self.matrix[i][j], end=" | ") # if it's not the ugly maxint then print it
+            print()
 
-from collections import defaultdict
-# Solves all pair shortest path
-# via Floyd Warshall Algorithm
-
-def floydWarshall(graph):
-
-	# dist[][] will be the output
-	# matrix that will finally
-	# 	have the shortest distances
-	# 	between every pair of vertices
-	#  initializing the solution matrix
-	# same as input graph matrix
-	# OR we can say that the initial
-	# values of shortest distances
-	# are based on shortest paths considering no
-	# intermediate vertices
-
-
-    dist = list(map(lambda i: list(map(lambda j: j, i)), graph))
-
-    # Add all vertices one by one
-	# to the set of intermediate
-	# vertices.
-	# ---> Before start of an iteration,
-	# we have shortest distances
-	# between all pairs of vertices
-	# such that the shortest
-	# distances consider only the
-	# vertices in the set
-	# {0, 1, 2, .. k-1} as intermediate vertices.
-	# ----> After the end of a
-	# iteration, vertex no. k is
-	# added to the set of intermediate
-	# vertices and the
-	# set becomes {0, 1, 2, .. k}
-
-    for k in range(V):
-        # pick all vertices as source one by one
-        for i in range(V):
-            # Pick all vertices as destination for the
-            # above picked source
-            for j in range(V):
-                # If vertex k is on the shortest path from
-                # i to j, then update the value of dist[i][j]
-                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
-    printSolution(dist)
+    def floydWarshall(self):
+        for k in range(self.vertixes):
+            # pick all vertices as source one by one
+            for i in range(self.vertixes):
+                # Pick all vertices as destination for the
+                # above picked source
+                for j in range(self.vertixes):
+                    # If vertex k is on the shortest path from
+                    # i to j, then update the value of dist[i][j]
+                    self.matrix[i][j] = min(self.matrix[i][j], self.matrix[i][k] + self.matrix[k][j])
 
 
-# A utility function to print the solution
-def printSolution(dist):
-    print("Following matrix shows the shortest distances between every pair of vertices")
-    for i in range(V):
-        for j in range(V):
-            if dist[i][j] == INF:
-                print("INF", end=" ")
-            else:
-                print(dist[i][j], end=" ")
-            if j == V - 1:
-                print("")
-        print("")
-
-
-
-
-# Driver program to test the above program
-# Let us create the following weighted graph
-'''
-			10
-	(0)------->(3)
-		|		 /|\
-	5 |		 |
-		|		 | 1
-	\|/		 |
-	(1)------->(2)
-			3		 '''
-graph = [[0, 5, INF, 10],
-		[INF, 0, 3, INF],
-		[INF, INF, 0, 1],
-		[INF, INF, INF, 0]
-		]
-# Print the solution
-floydWarshall(graph)
-# This code is contributed by Mythri J L
+g5 = Graph(4)
+g5.addEdge(0, 1, 5)
+g5.addEdge(0, 3, 10)
+g5.addEdge(1, 2, 3)
+g5.addEdge(2, 3, 1)
+g5.printGraph()
+g5.floydWarshall()
+g5.printGraph()
